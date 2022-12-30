9f039a614c38ac62177a68f7b5b7db4385c939c7
XeonBash <22185152+SubyiaLab@users.noreply.github.com>
2022-12-30 22:56:10 +0300

    Completed logic + ui

--- script.js
+++ script.js
@@ -1,54 +1,112 @@
 
-function playRound(playerSelection, computerSelection){
-    let result = '';
+let computerScore = 0;
+let playerScore = 0;
+let tieScore = 0;
+let options = ["rock","paper","scissors"];
+let result = '';
+
+const rockBtn = document.querySelector(".rock");
+const paperkBtn = document.querySelector(".paper");
+const scissBtn = document.querySelector(".scissors");
+
+function playRound(playerSelection){
+
+ 
+  const computerSelection = options[Math.floor(Math.random() * options.length)];
+  console.log(computerSelection);
+
   if ( playerSelection === 'rock' && computerSelection === 'rock'){
     result = "its tie!";
+    tieScore++;
+    scoreResult.innerHTML =  "this round winner is :" + result;
+    playerScoreTotal.innerHTML = "your score is : " + playerScore;
+    computerScoreTotal.innerHTML = "Computer score is :" + computerScore;
+    tieScoreTotal.innerHTML = "tie score is :" + tieScore;
+    
+
   }else if (playerSelection === 'rock' && computerSelection === 'paper') {
     result="computer wins!";
+    computerScore++;
+    scoreResult.innerHTML =  "this round winner is :" + result;
+    playerScoreTotal.innerHTML = "your score is : " + playerScore;
+    computerScoreTotal.innerHTML = "Computer score is :" + computerScore;
+    tieScoreTotal.innerHTML = "tie score is :" + tieScore;
+
   } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
     result="you win!";
+    playerScore++;
+    scoreResult.innerHTML =  "this round winner is :" + result;
+    playerScoreTotal.innerHTML = "your score is : " + playerScore;
+    computerScoreTotal.innerHTML = "Computer score is :" + computerScore;
+    tieScoreTotal.innerHTML = "tie score is :" + tieScore;
+
   } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
     result="computer wins!";
+    computerScore++;
+    scoreResult.innerHTML =  "this round winner is :" + result;
+    playerScoreTotal.innerHTML = "your score is : " + playerScore;
+    computerScoreTotal.innerHTML = "Computer score is :" + computerScore;
+    tieScoreTotal.innerHTML = "tie score is :" + tieScore;
+
   } else if (playerSelection === 'paper' && computerSelection === 'rock') {
     result='you win!';
+    playerScore++;
+    scoreResult.innerHTML =  "this round winner is :" + result;
+    playerScoreTotal.innerHTML = "your score is : " + playerScore;
+    computerScoreTotal.innerHTML = "Computer score is :" + computerScore;
+    tieScoreTotal.innerHTML = "tie score is :" + tieScore;
+
   } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
     result="computer wins!";
+    computerScore++;
+    scoreResult.innerHTML =  "this round winner is :" + result;
+    playerScoreTotal.innerHTML = "your score is : " + playerScore;
+    computerScoreTotal.innerHTML = "Computer score is :" + computerScore;
+    tieScoreTotal.innerHTML = "tie score is :" + tieScore;
+
   } else if (playerSelection === 'scissors' && computerSelection === 'paper'){
     result="you win!";
+    playerScore++;
+    scoreResult.innerHTML =  "this round winner is :" + result;
+    playerScoreTotal.innerHTML = "your score is : " + playerScore;
+    computerScoreTotal.innerHTML = "Computer score is :" + computerScore;
+    tieScoreTotal.innerHTML = "tie score is :" + tieScore;
+
   } else if (playerSelection === 'paper' && computerSelection === 'paper'){ 
     result="its tie!";
+    tieScore++;
+    scoreResult.innerHTML =  "this round winner is :" + result;
+    playerScoreTotal.innerHTML = "your score is : " + playerScore;
+    computerScoreTotal.innerHTML = "Computer score is :" + computerScore;
+    tieScoreTotal.innerHTML = "tie score is :" + tieScore;
+
   } else if (playerSelection === 'scissors' && computerSelection === 'scissors'){
     result="its tie!"
+    tieScore++;
+    scoreResult.innerHTML =  "this round winner is :" + result;
+    playerScoreTotal.innerHTML = "your score is : " + playerScore;
+    computerScoreTotal.innerHTML = "Computer score is :" + computerScore;
+    tieScoreTotal.innerHTML = "tie score is :" + tieScore;
   } else {
     result="invalid input";
   }
-  return result;
+  return result,playerScore,computerScore,tieScore;
 } 
 
-function game (){
-    let computerScore = 0;
-    let playerScore = 0;
-    let tieScore = 0;
-    let options = ["rock","paper","scissors"];
-    
-    for (let i=1; i<= 5; i++){
-        const computerSelection = options[Math.floor(Math.random() * options.length)];
-        console.log(computerSelection);
-        let playerSelection = prompt("let play!");
-        playerSelection = playerSelection.toLowerCase();
-        playRound(playerSelection, computerSelection);
-        console.log(playRound(playerSelection, computerSelection));
-        if (playRound(playerSelection, computerSelection) === 'you win!') {
-            playerScore++;
-        } else if (playRound(playerSelection, computerSelection) === "computer wins!"){
-            computerScore++;
-        } else {
-            tieScore++;
-        }
+let rock = 'rock';
+let paper = 'paper';
+let scissors = 'scissors';
+
+rockBtn.addEventListener('click',() => playRound('rock'));
+paperkBtn.addEventListener('click',() => playRound('paper'));
+scissBtn.addEventListener('click',() =>playRound('scissors'));
+
+const playerScoreTotal = document.querySelector(".player-score");
+const computerScoreTotal = document.querySelector(".computer-score");
+const tieScoreTotal = document.querySelector(".tie");
+const scoreResult = document.querySelector(".result");
+
+
+
         
-    }  
-    console.log("computer score is:" + computerScore);
-    console.log("player score is:" + playerScore);
-    console.log("ties :" + tieScore);
-}   
-game();
+
